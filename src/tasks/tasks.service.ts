fe3a76511123b5ceeadee9350d5357a30bff9e3f
unknown <rajeshroyal896@gmail.com>
2021-05-02 07:39:32 +0530

    Created tasks controller, module, service, dto, model

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    public getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    public getTaskById(id: string): Task {
+        return this.tasks.find((task) => task.id === id);
+    }
+
+    public createTask(createTaskDto: CreateTaskDto) : Task {
+        const { title, description } = createTaskDto;
+        const task: Task = {
+            id: uuid(),
+            title: title,
+            description: description,
+            status: TaskStatus.DONE
+        }
+        this.tasks.push(task);
+        return task;
+    }
+}
