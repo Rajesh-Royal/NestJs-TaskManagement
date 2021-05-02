0fc61403db72faec568262a0f8f6074ed6f24c0e
unknown <rajeshroyal896@gmail.com>
2021-05-02 19:34:39 +0530

    get all tasks or with filters

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query,
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
@@ -9,14 +10,10 @@ import { TasksService } from './tasks.service';
 export class TasksController {
     constructor(private tasksService: TasksService){}
 
-    // @Get()
-    // getAllTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-    //     if(Object.keys(filterDto).length){
-    //         return this.tasksService.getTasksWithFilters(filterDto);
-    //     }else{
-    //         return this.tasksService.getAllTasks();
-    //     }
-    // }
+    @Get()
+    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Promise<Task[]> {
+        return this.tasksService.getAllTasks(filterDto);
+    }
 
     @Get('/:id')
     getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task>{
@@ -34,8 +31,10 @@ export class TasksController {
         return this.tasksService.deleteTask(id)
     }
 
-    // @Patch('/:id/status')
-    // updateTask(@Param('id') id: string, @Body('status', TaskStatusValidationPipe) status: TaskStatus): Task {
-    //     return this.tasksService.updateTaskStatus(id, status);
-    // }
+    @Patch('/:id/status')
+    updateTask(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus): Promise<Task> {
+        return this.tasksService.updateTaskStatus(id, status);
+    }
 }
