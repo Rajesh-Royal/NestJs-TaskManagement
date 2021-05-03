c7225c59047ecae0a4cffdde14c62d49071d84a4
unknown <rajeshroyal896@gmail.com>
2021-05-03 13:20:23 +0530

    User Signup service, repository and controller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth.credentials.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -9,5 +10,7 @@ export class AuthService {
         private userRepository: UserRepository
     ){}
 
-
+    public async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+            return await this.userRepository.signUp(authCredentialsDto);
+    }
 }
