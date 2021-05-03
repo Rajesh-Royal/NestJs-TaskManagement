851f1a222df0266a3440841cd49cfd22890a04ec
unknown <rajeshroyal896@gmail.com>
2021-05-03 22:06:48 +0530

    Guarded all the tasks routes with AuthGuard() decorator

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
@@ -11,27 +12,32 @@ export class TasksController {
     constructor(private tasksService: TasksService){}
 
     @Get()
+    @UseGuards(AuthGuard())
     getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Promise<Task[]> {
         return this.tasksService.getAllTasks(filterDto);
     }
 
     @Get('/:id')
+    @UseGuards(AuthGuard())
     getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task>{
         return this.tasksService.getTaskById(id);
     }
 
     @Post()
+    @UseGuards(AuthGuard())
     @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
         return this.tasksService.createTask(createTaskDto);
     }
 
     @Delete('/:id')
+    @UseGuards(AuthGuard())
     deleteTask(@Param('id', ParseIntPipe) id: number): Promise<boolean> {
         return this.tasksService.deleteTask(id)
     }
 
     @Patch('/:id/status')
+    @UseGuards(AuthGuard())
     updateTask(
         @Param('id', ParseIntPipe) id: number,
         @Body('status', TaskStatusValidationPipe) status: TaskStatus): Promise<Task> {
